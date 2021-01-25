e1aefa887b916849a9964c6f543e5d0ee7394a59
Keith Tharp <keithstharp@gmail.com>
2021-01-24 20:30:03 -0500

    getting basic structure set up

---
+++ src/views/About/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function About(): JSX.Element {
+
+
+  return (
+
+
+    <div className="about-screen">
+
+      <h2 className="view-title">This is the Differentiator</h2>
+
+    </div>
+
+  )
+
+};
+export default About;
---
+++ src/views/Differentiator/Differentiator.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function Differentiator(): JSX.Element {
+
+
+  return (
+
+
+    <div className="about-screen">
+
+      <h2 className="view-title">The Differentiator App will be here!</h2>
+
+    </div>
+
+  )
+
+};
+export default Differentiator;
