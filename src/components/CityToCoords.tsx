63f64d216bfd985cb7f9ecd6db243993d4153b61
Keith Tharp <keithstharp@gmail.com>
2021-01-31 11:40:00 -0500

    both APIs working

--- src/components/CityToCoords.tsx
+++ src/components/CityToCoords.tsx
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios'
-import CityOptionButton from './CityOptionButton';
+// import CityOptionButton from './CityOptionButton';
 import './components.css';
-
 
 function CityToCoords(): JSX.Element {
 
-  const [coords, setCoords] = useState({
-    lat: "",
-    long: ""
-  });
-
+  const [dayLength, setDayLength] = useState(0);
   const [searchResults, setSearchResults] = useState<any>([])
-
   const [input, setInput] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
 
@@ -21,49 +15,45 @@ function CityToCoords(): JSX.Element {
 
     setSearchTerm(event.currentTarget.value.toLowerCase())
   }
-  const findCoords = () => {
 
+  const findCoords = () => {
     const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${searchTerm}&key=${process.env.REACT_APP_COORDS_API_PW}`;
     axios.get(apiUrl).then((res) => {
-
       setSearchResults(res.data.results)
-      // setCoords({ lat: res.data.results[0].geometry.lat, long: res.data.results[0].geometry.lng });
-      console.log(res);
+    });
+  }
+
+  const callSunTimes = (lat: number, lng: number) => {
+    const apiUrl = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
+    axios.get(apiUrl).then((res) => {
+      setDayLength(res.data.results.day_length)
+      console.log(res.data);
 
     });
-
-    console.log(searchResults);
-
-
   }
 
   return (
 
     <div>
-
       <div className="about-screen">
-        <input className="city-search-input" onChange={handleSearchChange} value={input} placeholder="Search Location" />
+        <input className="city-search-input m-3" onChange={handleSearchChange} value={input} placeholder="Search Location" />
         <button onClick={findCoords}>Search</button>
-        <p>Choose the correct location</p>
         {searchResults.length > 0 ?
-          <div className="row justify-content-center">
-            <div className="col-auto border border-primary">
+          <div className="row justify-content-center mt-2">
+            <div className="col-auto border border-secondary bg-light">
+              <h4 className="mt-2">Choose the correct location</h4>
               <ul className="city-list">
                 {searchResults.map(function (city: any, index: number) {
-                  return <li><button className="city-option-button" key={index}>+</button> {city.formatted}</li>
+                  return <li key={index}><button onClick={() => callSunTimes(city.geometry.lat, city.geometry.lng)} className="city-option-button">+</button> {city.formatted}</li>
                 })}
               </ul>
             </div>
           </div>
           : null}
-
+        <p>{dayLength !== 0 && dayLength}</p>
 
       </div>
-
     </div>
-
-
   )
-
 };
 export default CityToCoords;
